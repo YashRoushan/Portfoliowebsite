2c228562a318ce678fc4944587bfa0e1497c1e8f
yash roushan <yashroushanprogramming@gmail.com>
2025-04-21 15:21:33 -0300

    changed languages

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,7 +24,7 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> Java, C, PHP, Javascript and Python. </b>
+                <b className="purple"> Java, C, Javascript and Python. </b>
               </i>
               <br />
               <br />

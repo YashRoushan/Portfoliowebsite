64d8f216b58fc59cbbddad17773c35eceaa6270f
Yash Roushan <yashroushanprogramming@gmail.com>
2025-04-23 15:18:45 -0300

    updated interest

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,7 +14,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Developed by Yash Roushan</h3>
+          <h3>Yash Roushan</h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} YR</h3>
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -29,10 +29,10 @@ function Home2() {
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Web Technologies and Products </b>, <b className="purple">AI Automation</b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  Cloud.
                 </b>
               </i>
               <br />
